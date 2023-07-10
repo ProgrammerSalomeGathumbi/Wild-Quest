d97c7e4ee052bc7507897c01d84ec36d04583b45
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-07-10 14:21:38 +0300

    Added a new Assets folder containing all the important components of the game.

---
+++ Asset/Assets/Scripts/Animal/AnimalAttack.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class AnimalAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform woolPoint;
+    [SerializeField] private GameObject[] woolballs;
+
+    [Header("Sound")]
+    [SerializeField] private AudioSource woolSoundEffect;
+
+    private Animator anim;
+    private AnimalMovement animalMovement;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        animalMovement = GetComponent<AnimalMovement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && animalMovement.canAttack())
+            Attack();
+
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        
+        anim.SetTrigger("attack");
+        woolSoundEffect.Play();
+        cooldownTimer = 0;
+
+        woolballs[FindWoolball()].transform.position = woolPoint.position;
+        woolballs[FindWoolball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+    private int FindWoolball()
+    {
+        for (int i = 0; i < woolballs.Length; i++)
+        {
+            if (!woolballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+
+}
---
+++ Asset/Assets/Scripts/Health/HealthCollectible.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    [SerializeField] private float healthValue;
+    [Header("Sound")]
+    [SerializeField] private AudioSource collectSoundEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Animal")
+        {
+            collectSoundEffect.Play();
+            collision.GetComponent<Health>().AddHealth(healthValue);
+            gameObject.SetActive(false);
+        }
+    }
+}
